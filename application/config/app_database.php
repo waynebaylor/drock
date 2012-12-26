57a4bfeda7bbcdd2316241114116a886c0ead435
wtaylor <wtaylor@405721de-3cf0-4ad0-9f55-75e06b4147d4>
2012-12-26 05:11:42 +0000

    more stuff

---
+++ application/config/app_database.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * Put application database configuration here. This is where an application 
+ * can extend or override any database settings.
+ */
+
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'drock';
+
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_bin';
