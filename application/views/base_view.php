19117121056c480f1d8ab661e8dd5401c6192398
wtaylor <wtaylor@405721de-3cf0-4ad0-9f55-75e06b4147d4>
2012-12-26 05:29:38 +0000

    js and css

--- application/views/base_view.php
+++ application/views/base_view.php
@@ -6,11 +6,16 @@
 		<?php echo $title ?>
 	</title>
 	
-	<script type="text/javascript" src="<?php echo base_url('assets/jquery/1.8.3/jquery-1.8.3.js') ?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/2.2.1/css/bootstrap.css') ?>">
+	<link rel="stylesheet/less" type="text/css" href="<?php echo base_url('css/drock.less') ?>">
+	
+	<script type="text/javascript" src="<?php echo base_url('assets/less/less-1.3.1.min.js') ?>"></script>
 </head>
 <body>
 
 <?php echo $content ?>
 	
+	<script type="text/javascript" src="<?php echo base_url('assets/jquery/1.8.3/jquery-1.8.3.min.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/bootstrap/2.2.1/js/bootstrap.min.js') ?>"></script>
 </body>
 </html>
