ce97e1eba4245ea4e58f740b45db74dbc2740f81
wtaylor <wtaylor@405721de-3cf0-4ad0-9f55-75e06b4147d4>
2012-12-29 05:02:13 +0000

    login page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,7 +1,7 @@
 
 <div class="container">
 	<div class="row">
-		<div class="span6">
+		<div class="span2 offset4">
 			<h1>D-ROCK</h1>
 			<h4>Crush it!</h4>
 		</div>
@@ -12,6 +12,10 @@
 				
 				<label>Password</label>
 				<input type="password" name="password" value="">
+				
+				<div class="form-actions">
+					<button type="submit" class="btn">Login</button>
+				</div>
 			</form>
 		</div>
 	</div>
