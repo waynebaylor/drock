a01b0c8d243123842838a08e548ecc5d1628663d
wtaylor <wtaylor@405721de-3cf0-4ad0-9f55-75e06b4147d4>
2012-12-29 05:07:35 +0000

    login page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,22 +1,20 @@
 
-<div class="container">
-	<div class="row">
-		<div class="span2 offset4">
-			<h1>D-ROCK</h1>
-			<h4>Crush it!</h4>
+<form method="post" action="">
+	<div class="container">
+		<div class="row">
+			<div class="offset4 span2">
+				<h1>D-ROCK</h1>
+				<h4>Crush it!</h4>
+			</div>
+			<div class="span6">
+				<input type="text" class="input-block-level" placeholder="Username" name="username" value="">
+				<input type="password" class="input-block-level" placeholder="Password" name="password" value="">
+			</div>
 		</div>
-		<div class="span6">
-			<form method="post" action="">
-				<label>Username</label>
-				<input type="text" name="username" value="">
-				
-				<label>Password</label>
-				<input type="password" name="password" value="">
-				
-				<div class="form-actions">
-					<button type="submit" class="btn">Login</button>
-				</div>
-			</form>
+		<div class="row">
+			<div class="offset6 span6">
+				<button type="submit" class="btn">Login</button>
+			</div>
 		</div>
 	</div>
-</div>
+</form>
