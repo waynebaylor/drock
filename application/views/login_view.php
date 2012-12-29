62b6a2982b9332909137af787408e3b2166ee268
wtaylor <wtaylor@405721de-3cf0-4ad0-9f55-75e06b4147d4>
2012-12-29 04:41:21 +0000

    login page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,1 +1,14 @@
-make login page
+
+<div class="container">
+	<div class="row">
+		<div class="span8">
+			D-ROCK
+		</div>
+		<div class="span4">
+			<form method="post" action="">
+				<input type="text" placeholder="Username" value="">
+				<input type="password" placeholder="Password" value="">
+			</form>
+		</div>
+	</div>
+</div>
