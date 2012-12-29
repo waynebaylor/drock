d7ef46e2ac4549dc84a0da00a5fc718514550c7e
wtaylor <wtaylor@405721de-3cf0-4ad0-9f55-75e06b4147d4>
2012-12-29 04:54:41 +0000

    login page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,14 +1,17 @@
 
 <div class="container">
 	<div class="row">
-		<div class="span6 pull-right">
+		<div class="span6">
 			<h1>D-ROCK</h1>
 			<h4>Crush it!</h4>
 		</div>
 		<div class="span6">
 			<form method="post" action="">
-				<input type="text" placeholder="Username" value="">
-				<input type="password" placeholder="Password" value="">
+				<label>Username</label>
+				<input type="text" name="username" value="">
+				
+				<label>Password</label>
+				<input type="password" name="password" value="">
 			</form>
 		</div>
 	</div>
